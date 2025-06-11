4ac45ae2ca9196f6ded360182821584344d27991
Abhishek Das <abhishekdaszy@gmail.com>
2025-06-11 22:27:01 +0530

    added some modern ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DynamicTabs from './Components/DynamicTabs';
 
 
 const App = () => {
   return (
     <div>
       <h1> Dynamic Tabs </h1>
+      <DynamicTabs/>
     </div>
   )
 }
